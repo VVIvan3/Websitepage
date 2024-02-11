ce6280e427db7d82ecfd115bb15cbd4bcc75444b
VVIvan3 <vanya.vorobiev1@mail.ru>
2024-02-11 15:31:49 +0300

    Completed navigation and page-creationg for home

--- index.bundle.js
+++ index.bundle.js
@@ -7,26 +7,100 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/homePage.js":
+/*!*************************!*\
+  !*** ./src/homePage.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ getHomePage)\n/* harmony export */ });\n/* harmony import */ var _menuPage__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./menuPage */ \"./src/menuPage.js\");\n\n\nfunction getHomePage() {\n    const contentHolder = document.createElement('div');\n    const title = document.createElement('div');\n    const subtitle = document.createElement('div');\n    const homeMenuNav = document.createElement('button');\n\n    contentHolder.classList.add('home-content');\n    title.classList.add('home-title');\n    subtitle.classList.add('home-subtitle');\n    homeMenuNav.classList.add('home-menu-button');\n\n    title.textContent = 'Restaurant name';\n    subtitle.textContent = 'Cooking tasty food since 1907';\n    homeMenuNav.textContent = 'Our menu';\n    \n    title.appendChild(subtitle);\n    contentHolder.append(title, homeMenuNav);\n\n    return contentHolder;\n}\n\n\n//# sourceURL=webpack://websitepage/./src/homePage.js?");
+
+/***/ }),
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("const navInit = (() => {\n    const navBtns = document.querySelectorAll('.navbtn');\n    navBtns.forEach((btn) => {\n        btn.addEventListener('click', (event) => {\n            switch (event.target.id) {\n                case 'home':\n                    console.log('home')\n                    break;\n                case 'menu':\n                    break;\n                case 'about':\n                    break;\n            }\n        })\n    })\n})()\n\n//# sourceURL=webpack://websitepage/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _homePage__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./homePage */ \"./src/homePage.js\");\n/* harmony import */ var _menuPage__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./menuPage */ \"./src/menuPage.js\");\n\n\n\nconst contentBody = document.querySelector('#content');\n\nconst navInit = (() => {\n    const navBtns = document.querySelectorAll('.navbtn');\n    navBtns.forEach((btn) => {\n        btn.addEventListener('click', (event) => {\n            switch (event.target.id) {\n                case 'home':\n                    mainControls.loadPage((0,_homePage__WEBPACK_IMPORTED_MODULE_0__[\"default\"])());\n                    mainControls.setHomeMenuNav();\n                    break;\n                case 'menu':\n                    mainControls.loadPage((0,_menuPage__WEBPACK_IMPORTED_MODULE_1__[\"default\"])());\n                    break;\n                case 'about':\n                    break;\n            }\n        });\n    });\n})();\n\nclass mainControls {\n    static cleanContentBody() {\n        contentBody.innerHTML = '';\n    }\n    static loadPage(page) {\n        mainControls.cleanContentBody()\n        contentBody.appendChild(page);\n    }\n    static setHomeMenuNav() {\n        const target = document.querySelector('.home-menu-button');\n        target.addEventListener('click', () => {\n            mainControls.loadPage((0,_menuPage__WEBPACK_IMPORTED_MODULE_1__[\"default\"])())\n        });\n    }\n}\n\n//# sourceURL=webpack://websitepage/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/menuPage.js":
+/*!*************************!*\
+  !*** ./src/menuPage.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ getMenuPage)\n/* harmony export */ });\nfunction getMenuPage () {\n    console.log('succs')\n}\n\n//# sourceURL=webpack://websitepage/./src/menuPage.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
